6fbf03fbae0ecd638e41949ef5686fcf4f0c96f6
vstrkvpolina <polinabarret000@gmail.com>
2025-05-17 02:58:00 +0300

    fix app alerts

--- lib/utils/app_alerts.dart
+++ lib/utils/app_alerts.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
+import 'package:myfoodration/data/models/food.dart';
+import 'package:myfoodration/providers/food/food.dart';
 import 'package:myfoodration/utils/utils.dart';
+import 'package:go_router/go_router.dart';
 
 class AppAlerts {
   AppAlerts._();
@@ -16,5 +21,30 @@ class AppAlerts {
         backgroundColor: context.colorScheme.primary,
       ),
     );
+  }
+
+  static Future<void> showDeleteAlertDialog(
+      BuildContext context, WidgetRef ref, Food food) async {
+    Widget cancelButton =
+        TextButton(onPressed: () => context.pop(), child: const Text('нет'));
+    Widget deleteButton = TextButton(onPressed: () async{
+      await ref.read(foodProvider.notifier).deleteFood(food).then((value) {
+        AppAlerts.displaySnackBar(context, 'Еда');
+        context.pop();
+      });
+    }, child: const Text('да'));
+
+    AlertDialog alert = AlertDialog(
+      title: const Text('Уверены?'),
+      actions: [
+        deleteButton,
+        cancelButton,
+      ],
+    );
+    await showDialog(
+        context: context,
+        builder: (ctx) {
+          return alert;
+        });
   }
 }
